547673db73ca34b46f345dbc4cf88f597f380644
Denzil Rodrigues <denzil.rdz@gmail.com>
2023-07-01 19:32:30 +0530

    Added slides for work

--- src/components/work/index.tsx
+++ src/components/work/index.tsx
@@ -1,77 +1,114 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 import apartment from '@assets/img/apartment.gif';
 import cart from '@assets/img/cart.gif';
 import eventx from '@assets/img/eventx.gif';
+import imagesCompare from '@assets/img/imagesCompare.gif';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEye } from '@fortawesome/free-solid-svg-icons';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import Styles from './index.module.scss';
+import { Splide, SplideSlide } from '@splidejs/react-splide';
+import '@splidejs/react-splide/css';
 
 const Work: FC = () => {
+  const getWindowWidth = () => {
+    return window.innerWidth;
+  };
+
+  const [windowWidth, setWindowWidth] = useState(getWindowWidth());
+
+  useEffect(() => {
+    const handleWidthResize = () => {
+      console.log(getWindowWidth());
+      setWindowWidth(getWindowWidth());
+    };
+    window.addEventListener('resize', handleWidthResize);
+
+    return () => {
+      window.removeEventListener('resize', handleWidthResize);
+    };
+  }, []);
+
+  const workObj = [
+    {
+      label: 'Images Compare',
+      img: imagesCompare,
+      description:
+        'An open-source Python package allows for image comparison between two provided images using a specified threshold.',
+      link: 'https://pypi.org/project/images-compare/',
+      github: '',
+    },
+    {
+      label: 'EventX',
+      img: eventx,
+      description:
+        ' EVENTX is designed Proudly for Event Rental Businesses. This App encompasses spectrum of advanced functionalities. This is developed using React and firebase Realtime database.',
+      link: 'https://fourthrocktechnologies.in/',
+      github: '',
+    },
+    {
+      label: 'Cart',
+      img: cart,
+      description:
+        'This is not just another e-commerce website but is focused on the privacy of the user. The user is assigned a group where the sub-interest changes every three days and the main-interest every week. This project is developed using Django framework.',
+      link: '',
+      github: 'https://github.com/denzilrdz/My-Cart',
+    },
+    {
+      label: 'Apartment Management System',
+      img: apartment,
+      description:
+        'JSP system for day-to-day activities of apartment like bill payments, swimming and gym membership, hall booking, appointment for vacant apartment, service request for apartment.',
+      link: '',
+      github: 'https://github.com/denzilrdz/Apartment-Managment-System',
+    },
+  ];
   return (
     <>
       <section className={Styles.section} id='work' data-aos='fade-up'>
         <h2 className={Styles.sectionTitle}>Work</h2>
-
-        <div className={`${Styles.workContainer} ${Styles.bdGrid}`}>
-          <div className={Styles.workImg} data-aos='fade-up'>
-            <img src={eventx} alt='EventX' />
-            <div className={Styles.cardBody}>
-              <h4>EventX</h4>
-              <p className={Styles.workText}>
-                EVENTX is designed Proudly for Event Rental Businesses. This App
-                encompasses spectrum of advanced functionalities. This is
-                developed using React and firebase Realtime database.
-              </p>
-              <a
-                href='https://fourthrocktechnologies.in/'
-                target='_blank'
-                rel='noreferrer'
-                className={Styles.workIcon}
-              >
-                <FontAwesomeIcon size='sm' icon={faEye} />
-              </a>
-            </div>
-          </div>
-          <div className={Styles.workImg} data-aos='fade-up'>
-            <img src={cart} alt='Cart' />
-            <div className={Styles.cardBody}>
-              <h4>Cart</h4>
-              <p className={Styles.workText}>
-                This is not just another e-commerce website but is focused on
-                the privacy of the user. The user is assigned a group where the
-                sub-interest changes every three days and the main-interest
-                every week. This project is developed using Django framework.
-              </p>
-              <a
-                href='https://github.com/denzilrdz/My-Cart'
-                target='_blank'
-                rel='noreferrer'
-                className={Styles.workIcon}
-              >
-                <FontAwesomeIcon size='sm' icon={faGithub} />
-              </a>
-            </div>
-          </div>
-          <div className={Styles.workImg} data-aos='fade-up'>
-            <img src={apartment} alt='Apartment Management System' />
-            <div className={Styles.cardBody}>
-              <h4>Apartment Management System</h4>
-              <p className={Styles.workText}>
-                JSP system for day-to-day activities of apartment like bill
-                payments, swimming and gym membership, hall booking, appointment
-                for vacant apartment, service request for apartment.
-              </p>
-              <a
-                href='https://github.com/denzilrdz/Apartment-Managment-System'
-                target='_blank'
-                rel='noreferrer'
-                className={Styles.workIcon}
-              >
-                <FontAwesomeIcon size='sm' icon={faGithub} />
-              </a>
-            </div>
-          </div>
+        <div className={`${Styles.workContainer}`}>
+          <Splide
+            options={{
+              type: 'loop',
+              perPage: windowWidth > 768 ? 3 : 1,
+              focus: 'center',
+            }}
+          >
+            {workObj.map((ele) => (
+              <SplideSlide key={ele.label}>
+                <div className={Styles.cardBody}>
+                  <img
+                    className={Styles.workImg}
+                    src={ele.img}
+                    alt={ele.label}
+                  />
+                  <h4 className={Styles.workTitle}> {ele.label}</h4>
+                  <p className={Styles.workText}>{ele.description}</p>
+                  {ele.link && (
+                    <a
+                      href={ele.link}
+                      target='_blank'
+                      rel='noreferrer'
+                      className={Styles.workIcon}
+                    >
+                      <FontAwesomeIcon size='sm' icon={faEye} />
+                    </a>
+                  )}
+                  {ele.github && (
+                    <a
+                      href={ele.github}
+                      target='_blank'
+                      rel='noreferrer'
+                      className={Styles.workIcon}
+                    >
+                      <FontAwesomeIcon size='sm' icon={faGithub} />
+                    </a>
+                  )}
+                </div>
+              </SplideSlide>
+            ))}
+          </Splide>
         </div>
       </section>
     </>
